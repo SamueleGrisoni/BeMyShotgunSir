9c4aeb24cbb8f3a175e93c7e34760cfdec5a07cc
sslvo <69592192+sslvo@users.noreply.github.com>
2026-03-10 17:19:20 +0100

    feat: add CinemachineCamera for the Driver POV and Player prefab
    - Create Player prefab containing Sidecar, Camera and SphereMovement
    - Fixed Sphere physics: now it correctly rotate
    - Add CinamachineCamera to follows the Sidecar from the Driver POV
    - Add visual rotation of the sidecar dureing drift to give the player visual feedback

--- Assets/Scripts/Player/DriverController.cs
+++ Assets/Scripts/Player/DriverController.cs
@@ -5,6 +5,10 @@ public class DriverController : MonoBehaviour
     [SerializeField]
     private Rigidbody _sphereMovementRb;
     [SerializeField]
+    private Transform _visualModel;
+    [SerializeField]
+    private float _visualDriftMultiplier = 2.0f;
+    [SerializeField]
     private float _accelerationForce = 10f;
     [SerializeField]
     private float _brakingForce = 10f;
@@ -19,31 +23,29 @@ public class DriverController : MonoBehaviour
     [SerializeField]
     private AnimationCurve _steeringCurve;
     private SphereCollider _sphereMovementCollider;
-    // TODO temporanee
+    // TODO temporanee1
     private Input_Actions _inputActions;
     private Input_Actions.GameplayActions _gameplayActions;
     private bool _isGrounded;
-    private float _maxSpeed; // this is the actual max speed of the sidecar
-    private float CurrentForwardSpeed => transform.InverseTransformDirection(_sphereMovementRb.linearVelocity).z; // This is the real speed of the Sidecar along the z axis (with rispect to the local reference system).
+    private float _maxSpeed; // Actual max speed of the sidecar
+    private float CurrentForwardSpeed => transform.InverseTransformDirection(_sphereMovementRb.linearVelocity).z; // Real speed of the Sidecar along the z axis (with rispect to the local reference system).
     private void Awake()
     {
         _sphereMovementCollider = _sphereMovementRb.GetComponent<SphereCollider>();
-        _sphereMovementCollider.transform.parent = null; // Disjoint the sphere from the sidecar object
+        // TODO cercare un modo per toglierlo
+        _sphereMovementCollider.transform.parent = null; // Disjoint the sphere from the sidecar object. This avoid the SphereMovement to collide with colliders of the sidecar
         _inputActions = new Input_Actions();
         _inputActions.Enable();
         _gameplayActions = _inputActions.Gameplay;
 
         _maxSpeed = _maxSpeedNormal;
+        _isGrounded = true;
     }
-    private void Update()
-    {
-        transform.position = _sphereMovementRb.position;
-        // Update ground detection
-        GroundNormalRotation();
-    }
+    private void Update() => ApplyVisualDrift();
     private void FixedUpdate()
     {
-        // TODO da capire meglio questa riga
+        transform.position = _sphereMovementRb.transform.position; // Since all the physics computation are done in FixedUpdate this line is put here to avoid Jittering
+        //GroundNormalRotation();
         if (_isGrounded)
         {
             // Rotate sidecar
@@ -58,22 +60,23 @@ public class DriverController : MonoBehaviour
             Steer(steerInput);
 
             Vector3 currentVelocity = _sphereMovementRb.linearVelocity;
-            Vector3 targetVelocity = transform.forward * currentVelocity.magnitude;
+            var horizontalVelocity = new Vector3(currentVelocity.x, 0, currentVelocity.z);
+
+            Vector3 forwardDir = new Vector3(transform.forward.x, 0, transform.forward.z).normalized;
+            Vector3 targetVelocity = forwardDir * horizontalVelocity.magnitude;
+
             // if _gripFactor = 1f -> the velocity of the sphere tends to targetVelocity, this means that the direction of the velocity of the sphere is instanlty equals to the direction of the sidecar. So the sidecar doesn't derapa.
             // if _gripFactor = 0.1f -> the velocity of the sphere tends to currentVelocity, this means that the direction of the velocity of the sphere slowly tends to be equals to the direction of the sidecar. So the sidecar derapa.
             // TODO magari il smoothness coefficient lo faccio diventare una variabile
             // 5f is the smoothness coefficient that determins how fast or slow the velocity of the sphere tends to targetVelocity
-            _sphereMovementRb.linearVelocity = Vector3.Lerp(currentVelocity, targetVelocity, _gripFactor * Time.fixedDeltaTime * 2.5f);
+            var gripTargetVelocity = Vector3.Lerp(horizontalVelocity, targetVelocity, _gripFactor * Time.fixedDeltaTime * 5f);
+            Vector3 gripCorrection = gripTargetVelocity - horizontalVelocity;
+            _sphereMovementRb.AddForce(gripCorrection, ForceMode.VelocityChange);
 
-            // Limit to the lienar velocity of the sphere
-            if (Mathf.Abs(CurrentForwardSpeed) > _maxSpeed)
-            {
-                _sphereMovementRb.linearVelocity = transform.forward * Mathf.Clamp(CurrentForwardSpeed, -_maxSpeed, _maxSpeed);
-            }
-        }
-        else
-        {
-            //_rigidBody.AddForce(transform.up * -9.8f);
+            //if (Mathf.Abs(CurrentForwardSpeed) > _maxSpeed)
+            //{
+            //    _sphereMovementRb.linearVelocity = transform.forward * Mathf.Clamp(CurrentForwardSpeed, -_maxSpeed, _maxSpeed);
+            //}
         }
     }
     private void OnDestroy()
@@ -88,14 +91,11 @@ public class DriverController : MonoBehaviour
         if (accelInput > 0)
         {
             _sphereMovementRb.AddForce(transform.forward * (_accelerationForce * accelInput), ForceMode.Acceleration);
-            // ForceMode.Acceleration: interprets the parameter as acceleration and changes the velocity by the value of force * DT.
-            // The effect depends on the simulation step lenght but doesn't depend on the mass of the body.
         }
         else if (breakInput > 0)
         {
             _sphereMovementRb.AddForce(-transform.forward * (_brakingForce * breakInput), ForceMode.Acceleration);
         }
-
         // TODO apply ground resistance when the sidecar is offroad?
     }
 
@@ -111,8 +111,8 @@ public class DriverController : MonoBehaviour
 
     private void GroundNormalRotation()
     {
-        _isGrounded = Physics.SphereCast(_sphereMovementRb.position + transform.up * 0.1f, _sphereMovementCollider.radius, -transform.up, out RaycastHit hit);
-        //_isGrounded = Physics.Raycast(_rigidBody.position, -transform.up, out RaycastHit hit, 1f, _groundLayer);
+        //_isGrounded = Physics.SphereCast(_sphereMovementRb.position + transform.up * 0.1f, _sphereMovementCollider.radius, -transform.up, out RaycastHit hit);
+        _isGrounded = Physics.Raycast(_sphereMovementRb.position, -transform.up, out RaycastHit hit, 1f);
         if (_isGrounded)
         {
             // Allignement of the sidecar to the ground
@@ -120,5 +120,14 @@ public class DriverController : MonoBehaviour
                 Vector3.Cross(transform.right, hit.normal),
                 hit.normal);
         }
+    }
+
+    private void ApplyVisualDrift()
+    {
+        float lateralSpeed = -transform.InverseTransformDirection(_sphereMovementRb.linearVelocity).x;
+        float driftAngle = lateralSpeed * _visualDriftMultiplier;
+        _visualModel.localRotation = Quaternion.Euler(0, driftAngle, 0);
+        //Quaternion targetVisualRotation = Quaternion.Euler(0, driftAngle, 0);
+        //_visualModel.localRotation = Quaternion.Lerp(_visualModel.localRotation, targetVisualRotation, Time.deltaTime * 10f);
     }
 }
