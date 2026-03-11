0d43aba08404262385d6b0878a88a24f3e52ffe8
sslvo <69592192+sslvo@users.noreply.github.com>
2026-03-11 20:18:24 +0100

    feat: SOSidecarStats for configurable sidecar parameters
    - Add SOSidecarStats ScriptableObject to manage sidecar stats
    - Refactor DriverController to utilize SOSidecarStats
    - Created DefaultSidecar.asset to define default values for sidecar stats.
    - Modified Player prefab.

--- Assets/Scripts/Player/DriverController.cs
+++ Assets/Scripts/Player/DriverController.cs
@@ -2,50 +2,29 @@ using UnityEngine;
 
 public class DriverController : MonoBehaviour
 {
-    [SerializeField]
-    private Rigidbody _sphereMovementRb;
-    [SerializeField]
-    private Transform _visualModel;
-    [SerializeField]
-    private float _visualDriftMultiplier = 2.0f;
-    [SerializeField]
-    private float _accelerationForce = 10f;
-    [SerializeField]
-    private float _brakingForce = 10f;
-    [SerializeField]
-    private float _steeringSpeed = 90f;
-    [SerializeField]
-    private float _maxSpeedNormal = 25f; // _maxSpeedNormal is the max speed of the sidecar without boost;
-    [SerializeField]
-    private float _maxSpeedWithBoost = 35f;
-    [SerializeField]
-    private float _gripFactor = 0.9f;
-    [SerializeField]
-    private AnimationCurve _steeringCurve;
-    private SphereCollider _sphereMovementCollider;
-    // TODO temporanee1
+    [SerializeField] private SOSidecarStats _stats;
+    [SerializeField] private Rigidbody _movementBodyRb;
+    [SerializeField] private Transform _sidecar;
+    [SerializeField] private Transform _visualModel;
     private Input_Actions _inputActions;
     private Input_Actions.GameplayActions _gameplayActions;
     private bool _isGrounded;
     private float _maxSpeed; // Actual max speed of the sidecar
-    private float CurrentForwardSpeed => transform.InverseTransformDirection(_sphereMovementRb.linearVelocity).z; // Real speed of the Sidecar along the z axis (with rispect to the local reference system).
+    private float CurrentForwardSpeed => transform.InverseTransformDirection(_movementBodyRb.linearVelocity).z; // Real speed of the Sidecar along the z axis (with rispect to the local reference system).
     private void Awake()
     {
-        _sphereMovementCollider = _sphereMovementRb.GetComponent<SphereCollider>();
-        // TODO cercare un modo per toglierlo
-        _sphereMovementCollider.transform.parent = null; // Disjoint the sphere from the sidecar object. This avoid the SphereMovement to collide with colliders of the sidecar
         _inputActions = new Input_Actions();
         _inputActions.Enable();
         _gameplayActions = _inputActions.Gameplay;
 
-        _maxSpeed = _maxSpeedNormal;
+        _maxSpeed = _stats.MaxSpeedDefault;
         _isGrounded = true;
     }
     private void Update() => ApplyVisualDrift();
     private void FixedUpdate()
     {
-        transform.position = _sphereMovementRb.transform.position; // Since all the physics computation are done in FixedUpdate this line is put here to avoid Jittering
-        //GroundNormalRotation();
+        transform.position = _movementBodyRb.transform.position; // Since all the physics computation are done in FixedUpdate this line is put here to avoid Jittering
+        GroundNormalRotation();
         if (_isGrounded)
         {
             // Rotate sidecar
@@ -59,7 +38,7 @@ public class DriverController : MonoBehaviour
             // Apply steering forces
             Steer(steerInput);
 
-            Vector3 currentVelocity = _sphereMovementRb.linearVelocity;
+            Vector3 currentVelocity = _movementBodyRb.linearVelocity;
             var horizontalVelocity = new Vector3(currentVelocity.x, 0, currentVelocity.z);
 
             Vector3 forwardDir = new Vector3(transform.forward.x, 0, transform.forward.z).normalized;
@@ -67,21 +46,13 @@ public class DriverController : MonoBehaviour
 
             // if _gripFactor = 1f -> the velocity of the sphere tends to targetVelocity, this means that the direction of the velocity of the sphere is instanlty equals to the direction of the sidecar. So the sidecar doesn't derapa.
             // if _gripFactor = 0.1f -> the velocity of the sphere tends to currentVelocity, this means that the direction of the velocity of the sphere slowly tends to be equals to the direction of the sidecar. So the sidecar derapa.
-            // TODO magari il smoothness coefficient lo faccio diventare una variabile
-            // 5f is the smoothness coefficient that determins how fast or slow the velocity of the sphere tends to targetVelocity
-            var gripTargetVelocity = Vector3.Lerp(horizontalVelocity, targetVelocity, _gripFactor * Time.fixedDeltaTime * 5f);
+            var gripTargetVelocity = Vector3.Lerp(horizontalVelocity, targetVelocity, _stats.GripFactor * Time.fixedDeltaTime);
             Vector3 gripCorrection = gripTargetVelocity - horizontalVelocity;
-            _sphereMovementRb.AddForce(gripCorrection, ForceMode.VelocityChange);
-
-            //if (Mathf.Abs(CurrentForwardSpeed) > _maxSpeed)
-            //{
-            //    _sphereMovementRb.linearVelocity = transform.forward * Mathf.Clamp(CurrentForwardSpeed, -_maxSpeed, _maxSpeed);
-            //}
+            _movementBodyRb.AddForce(gripCorrection, ForceMode.VelocityChange);
         }
     }
     private void OnDestroy()
     {
-        // TODO Temporaneo
         _inputActions.Disable();
         _inputActions.Dispose();
     }
@@ -90,42 +61,40 @@ public class DriverController : MonoBehaviour
     {
         if (accelInput > 0)
         {
-            _sphereMovementRb.AddForce(transform.forward * (_accelerationForce * accelInput), ForceMode.Acceleration);
+            _movementBodyRb.AddForce(transform.forward * (_stats.AccelerationForce * accelInput), ForceMode.Acceleration);
         }
         else if (breakInput > 0)
         {
-            _sphereMovementRb.AddForce(-transform.forward * (_brakingForce * breakInput), ForceMode.Acceleration);
+            _movementBodyRb.AddForce(-transform.forward * (_stats.BrakingForce * breakInput), ForceMode.Acceleration);
         }
-        // TODO apply ground resistance when the sidecar is offroad?
     }
 
     private void Steer(float steerInput)
     {
         // Calculate speed-based steering modifier
         float speedRatio = Mathf.Abs(CurrentForwardSpeed) / _maxSpeed;
-        float steeringMultiplier = _steeringCurve.Evaluate(speedRatio);
+        float steeringMultiplier = _stats.SteeringCurve.Evaluate(speedRatio);
 
-        float adjustedTurnSpeed = _steeringSpeed * steeringMultiplier;
+        float adjustedTurnSpeed = _stats.SteeringSpeed * steeringMultiplier;
         transform.Rotate(transform.up, steerInput * adjustedTurnSpeed * Time.fixedDeltaTime);
     }
 
     private void GroundNormalRotation()
     {
-        //_isGrounded = Physics.SphereCast(_sphereMovementRb.position + transform.up * 0.1f, _sphereMovementCollider.radius, -transform.up, out RaycastHit hit);
-        _isGrounded = Physics.Raycast(_sphereMovementRb.position, -transform.up, out RaycastHit hit, 1f);
+        _isGrounded = Physics.Raycast(_movementBodyRb.position, -transform.up, out RaycastHit hit, 1f);
         if (_isGrounded)
         {
-            // Allignement of the sidecar to the ground
-            transform.rotation = Quaternion.LookRotation(
+            var targetRotation = Quaternion.LookRotation(
                 Vector3.Cross(transform.right, hit.normal),
                 hit.normal);
+            _sidecar.rotation = Quaternion.Lerp(transform.rotation, targetRotation, _stats.GroundAlignSpeed * Time.fixedDeltaTime);
         }
     }
 
     private void ApplyVisualDrift()
     {
-        float lateralSpeed = -transform.InverseTransformDirection(_sphereMovementRb.linearVelocity).x;
-        float driftAngle = lateralSpeed * _visualDriftMultiplier;
+        float lateralSpeed = -transform.InverseTransformDirection(_movementBodyRb.linearVelocity).x;
+        float driftAngle = lateralSpeed * _stats.VisualDriftMultiplier;
         _visualModel.localRotation = Quaternion.Euler(0, driftAngle, 0);
         //Quaternion targetVisualRotation = Quaternion.Euler(0, driftAngle, 0);
         //_visualModel.localRotation = Quaternion.Lerp(_visualModel.localRotation, targetVisualRotation, Time.deltaTime * 10f);
