7a6e0e0a41e4c93c991fab015fd4829078a3f169
sslvo <69592192+sslvo@users.noreply.github.com>
2026-03-14 19:08:32 +0100

    refactor: changed physics of the DriverController and add Driving State

---
+++ Assets/BeMyShotgunSir/Scripts/Gameplay/Player/DriftingDrivingState.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+namespace BeMyShotgunSir.Scripts.Gameplay.Player
+{
+    public class DriftingDrivingState : IDrivingState
+    {
+        public void Enter(DriverController controller) { }
+        public void ExecuteUpdate(DriverController controller)
+        {
+            if (!controller.IsGrounded)
+            {
+                controller.ChangeState(controller.AirState);
+            }
+
+            if (!controller.IsDriftingButtonPressed)
+            {
+                controller.ChangeState(controller.NormalState);
+            }
+        }
+        public void ExecuteFixedUpdate(DriverController controller)
+        {
+            controller.ApplyAcceleration(controller.ParentTransform.forward);
+            controller.ApplyGravity(controller.Stats.Gravity);
+
+            float steerControl = controller.DriftDirection == 1
+                ? Mathf.Lerp(0f, 2f, Mathf.InverseLerp(-1f, 1f, controller.SteerInput))
+                : Mathf.Lerp(2f, 0f, Mathf.InverseLerp(-1f, 1f, controller.SteerInput));
+            controller.ApplySteering(steerControl * controller.DriftDirection);
+
+            float driftControl = controller.DriftDirection == 1
+                ? Mathf.Lerp(0.5f, 2f, Mathf.InverseLerp(-1f, 1f, controller.SteerInput))
+                : Mathf.Lerp(2f, 0.5f, Mathf.InverseLerp(-1f, 1f, controller.SteerInput));
+            controller.AnimateSidecar(Quaternion.Euler(0, controller.DriftDirection * driftControl * controller.Stats.SteerAngularRotation, 0));
+        }
+        public void Exit(DriverController controller) { }
+    }
+}
