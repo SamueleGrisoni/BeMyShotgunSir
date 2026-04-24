a180734ce3894662f3ba2489bda1ae7b74ca5a8e
Marco Moretta <omegamorello@gmail.com>
2026-04-24 10:51:36 +0200

    feat(ui): add ui manager + minor fix

--- Assets/BeMyShotgunSir/Scripts/UI/UIManager.cs
+++ Assets/BeMyShotgunSir/Scripts/UI/UIManager.cs
@@ -1,16 +1,82 @@
 using UnityEngine;
 
-public class UIManager : MonoBehaviour
+
+namespace BeMyShotgunSir.Scripts.UI
+
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public enum UIScreen
     {
-        
+        CentralHub,
+        LobbyMenu,
+        SettingsMenu,
+        CustomizationMenu,
+        ShopMenu,
+        HUD
     }
 
-    // Update is called once per frame
-    void Update()
+    public enum UIOverlay
     {
-        
+        Achievements,
+        Leaderboards,
+        PrivateLobby
+    }
+
+    public class UIManager : MonoBehaviour
+    {
+        [Header("UI Controllers")]
+        [Header("Central Hub")]
+        [SerializeField] private CentralHubController _centralHubController;
+        [Header("Lobby Menu")]
+        [SerializeField] private LobbyViewController _lobbyViewController;
+
+
+        [Header("HUD")]
+        [SerializeField] private HUDController _hudController;
+
+        #region Game Objects
+        private GameObject _centralHubGO;
+        private GameObject _lobbyMenuGO;
+        private GameObject _hudGO;
+        #endregion
+
+        private void Awake()
+        {
+            _centralHubGO = _centralHubController.gameObject;
+            _lobbyMenuGO = _lobbyViewController.gameObject;
+            _hudGO = _hudController.gameObject;
+        }
+
+        private void Start()
+        {
+            ShowScreen(UIScreen.CentralHub, true);
+        }
+
+        public void ShowScreen(UIScreen screen, bool show)
+        {
+            switch (screen)
+            {
+                case UIScreen.CentralHub:
+                    _centralHubController.Show(show);
+                    HideAllScreensExcept(centralHub: true);
+                    break;
+                case UIScreen.LobbyMenu:
+                    _lobbyViewController.Show(show);
+                    HideAllScreensExcept(lobbyMenu: true);
+                    break;
+                case UIScreen.HUD:
+                    // _hudController.Show(show);
+                    break;
+                default:
+                    Debug.LogWarning("Unknown screen: " + screen);
+                    break;
+            }
+        }
+
+        private void HideAllScreensExcept(bool centralHub = false, bool lobbyMenu = false, bool hud = false)
+        {
+            if (!centralHub) _centralHubController.Show(false);
+            if (!lobbyMenu) _lobbyViewController.Show(false);
+            // if (!hud) _hudController.Show(false);
+        }
     }
 }
