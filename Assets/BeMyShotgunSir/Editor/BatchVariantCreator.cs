84d75a6d6503d27086c6d6207829b3a6e3c47641
dedepivot <131958127+dedepivot@users.noreply.github.com>
2026-04-25 16:35:42 +0200

    refactor: create prefab variant for building and city props

---
+++ Assets/BeMyShotgunSir/Editor/BatchVariantCreator.cs
@@ -0,0 +1,33 @@
+﻿using BeMyShotgunSir.Scripts.Gameplay.Track.Environment;
+using UnityEditor;
+using UnityEngine;
+
+namespace BeMyShotgunSir.Editor
+{
+    public class BatchVariantCreator
+    {
+        [MenuItem("Tools/Create Variants and Add Script")]
+        public static void CreateVariants()
+        {
+            // Target save path
+            string targetFolder = "Assets/BeMyShotgunSir/Demos/Track Generation/Prefabs/Props";
+
+            GameObject[] selectedObjects = Selection.GetFiltered<GameObject>(SelectionMode.Assets);
+
+            foreach (GameObject obj in selectedObjects)
+            {
+                if (PrefabUtility.GetPrefabAssetType(obj) == PrefabAssetType.NotAPrefab) continue;
+                string newPath = $"{targetFolder}/{obj.name}_Variant.prefab";
+
+                GameObject instance = (GameObject)PrefabUtility.InstantiatePrefab(obj);
+                instance.AddComponent<CityProps>();
+
+                PrefabUtility.SaveAsPrefabAsset(instance, newPath);
+                Object.DestroyImmediate(instance);
+            }
+
+            AssetDatabase.Refresh();
+            Debug.Log($"Successfully saved {selectedObjects.Length} variants to {targetFolder}!");
+        }
+    }
+}
