0296316b5b64a2e4c42b46e06878d3420bfe1ec8
dedepivot <131958127+dedepivot@users.noreply.github.com>
2026-04-28 13:35:44 +0200

    feat: add basic item spawning system

---
+++ Assets/BeMyShotgunSir/Scripts/Gameplay/Track/Items/Item.cs
@@ -0,0 +1,8 @@
+﻿namespace BeMyShotgunSir.Scripts.Gameplay.Track.Items
+{
+    public enum ItemType { POWERUP, OBSTACLE }
+    public class Item
+    {
+
+    }
+}
---
+++ Assets/BeMyShotgunSir/Scripts/Gameplay/Track/Items/SOItem.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace BeMyShotgunSir.Scripts.Gameplay.Track.Items
+{
+    [CreateAssetMenu(fileName = "ItemSO", menuName = "Be My Shotgun, Sir!/Items")]
+    public class SOItem : ScriptableObject
+    {
+        [Header("Power-Up Spawn Settings")]
+        [Tooltip("Chance to spawn a powerUp in a given Area Group.")]
+        [field: SerializeField] public int ChanceToSpawnPowerUpPerAreaGroup { get; private set; }
+
+        [Tooltip("Max number of Area Groups that can have PowerUp in a given Road Chunk.")]
+        [field: SerializeField] public int MaxAreaGroupsWithPowerUpPerRoadChunk { get; private set; }
+
+        [Header("Obstacle Spawn Settings")]
+        [Tooltip("Chance to spawn an obstacle in a given Area Group.")]
+        [field: SerializeField] public int ChanceToSpawnObstaclePerAreaGroup { get; private set; }
+
+        [Tooltip("Max number of Area Groups that can have Obstacles in a given Road Chunk.")]
+        [field: SerializeField] public int MaxAreaGroupsWithObstaclePerRoadChunk { get; private set; }
+    }
+}
